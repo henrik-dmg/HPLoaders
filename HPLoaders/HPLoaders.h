00e3fc9466c33770b5941b96ead8921518ad9451
Henrik Panhans <henrikpanhans@icloud.com>
2018-02-16 17:22:30 +0100

    Initial Commit

---
+++ HPLoaders/HPLoaders.h
@@ -0,0 +1,19 @@
+//
+//  HPLoaders.h
+//  HPLoaders
+//
+//  Created by Henrik Panhans on 16.02.18.
+//  Copyright © 2018 Henrik Panhans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HPLoaders.
+FOUNDATION_EXPORT double HPLoadersVersionNumber;
+
+//! Project version string for HPLoaders.
+FOUNDATION_EXPORT const unsigned char HPLoadersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HPLoaders/PublicHeader.h>
+
+
